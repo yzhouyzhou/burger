cce92786fa9dbcaf77af8bf3d41c5cd4ad1e4e8c
Ying Zhou <yzhouyzhou@yahoo.com>
2019-08-01 00:02:15 -0500

    modify routes

--- db/schema.sql
+++ db/schema.sql
@@ -8,6 +8,6 @@ CREATE TABLE burgers
 (
 	id int NOT NULL AUTO_INCREMENT,
 	burger_name varchar(50) NOT NULL,
-    devoured boolean DEFAULT false,
+    devoured boolean DEFAULT 0,
 	PRIMARY KEY (id)
 );
